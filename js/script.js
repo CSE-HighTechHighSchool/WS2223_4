7d125cbbe37dfd19d7b79b801bcf3944f843878d
Anish Pallati <anishp0828@gmail.com>
2022-10-24 13:15:40 -0400

    feat: navbar-boggler

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 let setTheme = localStorage.getItem("theme") ?? "light";
 
-document.documentElement.setAttribute("data-theme", setTheme);
+document.documentElement.setAttribute("theme", setTheme);
 
 document.querySelectorAll(".switch").forEach((button) => {
 	button.addEventListener("click", () => {
 		setTheme = setTheme === "light" ? "dark" : "light";
 
-		document.documentElement.setAttribute("data-theme", setTheme);
+		document.documentElement.setAttribute("theme", setTheme);
 		localStorage.setItem("theme", setTheme);
 	});
 });
