547815dd2197f2badb3c75c488a36bdf003d17b9
ANSirotkin <ansirotkin@icloud.com>
2022-10-26 13:01:16 -0400

    made iapetus moon

--- js/moons.js
+++ js/moons.js
@@ -3,91 +3,123 @@ const titan_text = document.querySelector(".titan_text");
 const cova = document.querySelector(".cova_t");
 const titan_alt = document.querySelector(".titan_alt");
 const titan_alt_p = document.querySelector(".titan_alt_p");
+
 const enc = document.querySelector(".enceladus");
 const enc_text = document.querySelector(".enceladus_text");
 const cova_e = document.querySelector(".cova_e");
 const enc_alt = document.querySelector(".enceladus_alt");
 const enc_alt_p = document.querySelector(".enceladus_alt_p");
 
+const iap = document.querySelector(".iapetus");
+const iap_text = document.querySelector(".iapetus_text");
+const cova_i = document.querySelector(".cova_i");
+const iap_alt = document.querySelector(".iapetus_alt");
+const iap_alt_p = document.querySelector(".iapetus_alt_p");
+
+//click effect for Titan
 let counter_t = 0;
-
-// document.querySelectorAll(".shrink_right").forEach(() => {
-
-// });
-
 titan.addEventListener("click", () => {
   if (counter_t % 2 === 0) {
-    titan.style.setProperty(
-      "--move", "translate(-22.5vw) scale(0.7, 0.7)"
-    );
-    cova.style.setProperty(
-      "--move", "translate(-22.5vw) scale(0.7, 0.7)"
-    );
-    titan_text.style.setProperty("--opac", "1");
-    titan_text.style.setProperty("--out", "translateX(40%)");
+    titan.style.transform = "translate(-22.5vw) scale(0.7, 0.7)";
+    cova.style.transform = "translate(-22.5vw) scale(0.7, 0.7)";
+    titan_text.style.opacity = "1";
+    titan_text.style.transform = "translateX(40%)";
   }
   else {
-    titan.style.setProperty("--move", "");
-    cova.style.setProperty("--move", "");
-    titan_text.style.setProperty("--opac", "0");
-    titan_text.style.setProperty("--out", "");
+    titan.style.transform = "";
+    cova.style.transform = "";
+    titan_text.style.opacity = "0";
+    titan_text.style.transform = "";
   }
   counter_t++;
 })
 
 titan_alt.addEventListener("click", () => {
   if (counter_t % 2 === 0) {
-    titan_alt.style.setProperty("--bg-rd", "50px");
-    titan_alt.style.setProperty("--bshad", "none");
-    titan_alt_p.style.setProperty("--bg-rd", "50px");
-    titan_alt_p.style.setProperty("--popc", "1");
-    titan_alt_p.style.setProperty("backdrop-filter", "blur(10px)");
+    titan_alt.style.borderRadius = "50px";
+    titan_alt.style.boxShadow = "none";
+    titan_alt_p.style.borderRadius = "50px";
+    titan_alt_p.style.opacity = "1";
+    titan_alt_p.style.backdropFilter = "blur(10px)";
   }
   else {
-    titan_alt.style.setProperty("--bg-rd", "50%");
-    titan_alt.style.setProperty("--bshad", "inset -10px -5px 10px black, inset 10px 5px 20px white");
-    titan_alt_p.style.setProperty("--bg-rd", "50%");
-    titan_alt_p.style.setProperty("--popc", "0");
-    titan_alt_p.style.setProperty("backdrop-filter", "none");
+    titan_alt.style.borderRadius = "50%";
+    titan_alt.style.boxShadow = "inset -10px -5px 10px black, inset 10px 5px 20px white";
+    titan_alt_p.style.borderRadius = "50%";
+    titan_alt_p.style.opacity = "0";
+    titan_alt_p.style.backdropFilter = "none";
   }
   counter_t++;
 })
 
+//click effect for Enceladus
 let counter_e = 0;
 enc.addEventListener("click", () => {
   if (counter_e % 2 === 0) {
-    enc.style.setProperty(
-      "--move", "translate(22.5vw) scale(0.7, 0.7)"
-    );
-    cova_e.style.setProperty(
-      "--move", "translate(22.5vw) scale(0.7, 0.7)"
-    );
-    enc_text.style.setProperty("--opac", "1");
-    enc_text.style.setProperty("--out", "translateX(-40%)");
+    enc.style.transform = "translate(22.5vw) scale(0.7, 0.7)";
+    cova_e.style.transform = "translate(22.5vw) scale(0.7, 0.7)";
+    enc_text.style.opacity = "1";
+    enc_text.style.transform = "translateX(-40%)";
   }
   else {
-    enc.style.setProperty("--move", "");
-    cova_e.style.setProperty("--move", "");
-    enc_text.style.setProperty("--opac", "0");
-    enc_text.style.setProperty("--out", "");
+    enc.style.transform = "";
+    cova_e.style.transform = "";
+    enc_text.style.opacity = "0";
+    enc_text.style.transform = "";
   }
   counter_e++;
 })
 
 enc_alt.addEventListener("click", () => {
   if (counter_e % 2 === 0) {
-    enc_alt.style.setProperty("--bg-rd", "50px");
-    enc_alt.style.setProperty("--bshad", "none");
-    enc_alt_p.style.setProperty("--bg-rd", "50px");
-    enc_alt_p.style.setProperty("--popc", "1");
-    enc_alt_p.style.setProperty("backdrop-filter", "blur(10px)");
+    enc_alt.style.borderRadius = "50px";
+    enc_alt.style.boxShadow = "none";
+    enc_alt_p.style.borderRadius = "50px";
+    enc_alt_p.style.opacity = "1";
+    enc_alt_p.style.backdropFilter = "blur(10px)";
   }
   else {
-    enc_alt.style.setProperty("--bg-rd", "50%");
-    enc_alt.style.setProperty("--bshad", "inset -10px -5px 10px black, inset 10px 5px 20px white");
-    enc_alt_p.style.setProperty("--bg-rd", "50%");
-    enc_alt_p.style.setProperty("--popc", "0");
-    enc_alt_p.style.setProperty("backdrop-filter", "none");
+    enc_alt.style.borderRadius = "50%";
+    enc_alt.style.boxShadow = "inset -10px -5px 10px black, inset 10px 5px 20px white";
+    enc_alt_p.style.borderRadius = "50%";
+    enc_alt_p.style.opacity = "0";
+    enc_alt_p.style.backdropFilter = "none";
   }
   counter_e++;
+})
+
+//click effect for Iapetus
+let counter_i = 0;
+iap.addEventListener("click", () => {
+  if (counter_i % 2 === 0) {
+    iap.style.transform = "translate(-22.5vw) scale(0.7, 0.7)";
+    cova_i.style.transform = "translate(-22.5vw) scale(0.7, 0.7)";
+    iap_text.style.opacity = "1";
+    iap_text.style.transform = "translateX(40%)";
+  }
+  else {
+    iap.style.transform = "";
+    cova_i.style.transform = "";
+    iap_text.style.opacity = "0";
+    iap_text.style.transform = "";
+  }
+  counter_i++;
+})
+
+iap_alt.addEventListener("click", () => {
+  if (counter_i % 2 === 0) {
+    iap_alt.style.borderRadius = "50px";
+    iap_alt.style.boxShadow = "none";
+    iap_alt_p.style.borderRadius = "50px";
+    iap_alt_p.style.opacity = "1";
+    iap_alt_p.style.backdropFilter = "blur(10px)";
+  }
+  else {
+    iap_alt.style.borderRadius = "50%";
+    iap_alt.style.boxShadow = "inset -10px -5px 10px black, inset 10px 5px 20px white";
+    iap_alt_p.style.borderRadius = "50%";
+    iap_alt_p.style.opacity = "0";
+    iap_alt_p.style.backdropFilter = "none";
+  }
+  counter_i++;
 })
