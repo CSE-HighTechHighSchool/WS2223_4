52592bbdff3c7107d41d96ac665cf9b2d18d1b76
Anish Pallati <anishp0828@gmail.com>
2022-11-01 20:00:16 -0400

    feat: Add destinations page and update linsk

--- js/moons.js
+++ js/moons.js
@@ -26,169 +26,169 @@ const mimas_alt_p = document.querySelector(".mimas_alt_p");
 //click effect for Titan
 let counter_t = 0;
 titan.addEventListener("click", () => {
-  if (counter_t % 2 === 0) {
-    titan.style.transform = "translate(-22.5vw) scale(0.7, 0.7)";
-    titan_text.style.opacity = "1";
-    titan_text.style.transform = "translateX(55%) scale(1.2, 1.2)";
-  }
-  else {
-    titan.style.transform = "";
-    titan_text.style.opacity = "0";
-    titan_text.style.transform = "";
-  }
-  counter_t++;
+	if (counter_t % 2 === 0) {
+		titan.style.transform = "translate(-22.5vw) scale(0.7, 0.7)";
+		titan_text.style.opacity = "1";
+		titan_text.style.transform = "translateX(55%) scale(1.2, 1.2)";
+	}
+	else {
+		titan.style.transform = "";
+		titan_text.style.opacity = "0";
+		titan_text.style.transform = "";
+	}
+	counter_t++;
 })
 
 titan_alt.addEventListener("click", () => {
-  if (counter_t % 2 === 0) {
-    titan_alt.style.borderRadius = "50px";
-    titan_alt.style.boxShadow = "none";
-    titan_alt_p.style.borderRadius = "50px";
-    titan_alt_p.style.opacity = "1";
-    titan_alt_p.style.backdropFilter = "blur(10px)";
-  }
-  else {
-    titan_alt.style.borderRadius = "50%";
-    titan_alt.style.boxShadow = "inset -10px -5px 10px black, inset 10px 5px 20px white";
-    titan_alt_p.style.borderRadius = "50%";
-    titan_alt_p.style.opacity = "0";
-    titan_alt_p.style.backdropFilter = "none";
-  }
-  counter_t++;
+	if (counter_t % 2 === 0) {
+		titan_alt.style.borderRadius = "50px";
+		titan_alt.style.boxShadow = "none";
+		titan_alt_p.style.borderRadius = "50px";
+		titan_alt_p.style.opacity = "1";
+		titan_alt_p.style.backdropFilter = "blur(10px)";
+	}
+	else {
+		titan_alt.style.borderRadius = "50%";
+		titan_alt.style.boxShadow = "inset -10px -5px 10px black, inset 10px 5px 20px white";
+		titan_alt_p.style.borderRadius = "50%";
+		titan_alt_p.style.opacity = "0";
+		titan_alt_p.style.backdropFilter = "none";
+	}
+	counter_t++;
 })
 
 //click effect for Enceladus
 let counter_e = 0;
 enc.addEventListener("click", () => {
-  if (counter_e % 2 === 0) {
-    enc.style.transform = "translate(22.5vw) scale(0.7, 0.7)";
-    enc_text.style.opacity = "1";
-    enc_text.style.transform = "translateX(-55%) scale(1.2, 1.2)";
-  }
-  else {
-    enc.style.transform = "";
-    enc_text.style.opacity = "0";
-    enc_text.style.transform = "";
-  }
-  counter_e++;
+	if (counter_e % 2 === 0) {
+		enc.style.transform = "translate(22.5vw) scale(0.7, 0.7)";
+		enc_text.style.opacity = "1";
+		enc_text.style.transform = "translateX(-55%) scale(1.2, 1.2)";
+	}
+	else {
+		enc.style.transform = "";
+		enc_text.style.opacity = "0";
+		enc_text.style.transform = "";
+	}
+	counter_e++;
 })
 
 enc_alt.addEventListener("click", () => {
-  if (counter_e % 2 === 0) {
-    enc_alt.style.borderRadius = "50px";
-    enc_alt.style.boxShadow = "none";
-    enc_alt_p.style.borderRadius = "50px";
-    enc_alt_p.style.opacity = "1";
-    enc_alt_p.style.backdropFilter = "blur(10px)";
-  }
-  else {
-    enc_alt.style.borderRadius = "50%";
-    enc_alt.style.boxShadow = "inset -10px -5px 10px black, inset 10px 5px 20px white";
-    enc_alt_p.style.borderRadius = "50%";
-    enc_alt_p.style.opacity = "0";
-    enc_alt_p.style.backdropFilter = "none";
-  }
-  counter_e++;
+	if (counter_e % 2 === 0) {
+		enc_alt.style.borderRadius = "50px";
+		enc_alt.style.boxShadow = "none";
+		enc_alt_p.style.borderRadius = "50px";
+		enc_alt_p.style.opacity = "1";
+		enc_alt_p.style.backdropFilter = "blur(10px)";
+	}
+	else {
+		enc_alt.style.borderRadius = "50%";
+		enc_alt.style.boxShadow = "inset -10px -5px 10px black, inset 10px 5px 20px white";
+		enc_alt_p.style.borderRadius = "50%";
+		enc_alt_p.style.opacity = "0";
+		enc_alt_p.style.backdropFilter = "none";
+	}
+	counter_e++;
 })
 
 //click effect for Iapetus
 let counter_i = 0;
 iap.addEventListener("click", () => {
-  if (counter_i % 2 === 0) {
-    iap.style.transform = "translate(-22.5vw) scale(0.7, 0.7)";
-    iap_text.style.opacity = "1";
-    iap_text.style.transform = "translateX(55%) scale(1.2, 1.2)";
-  }
-  else {
-    iap.style.transform = "";
-    iap_text.style.opacity = "0";
-    iap_text.style.transform = "";
-  }
-  counter_i++;
+	if (counter_i % 2 === 0) {
+		iap.style.transform = "translate(-22.5vw) scale(0.7, 0.7)";
+		iap_text.style.opacity = "1";
+		iap_text.style.transform = "translateX(55%) scale(1.2, 1.2)";
+	}
+	else {
+		iap.style.transform = "";
+		iap_text.style.opacity = "0";
+		iap_text.style.transform = "";
+	}
+	counter_i++;
 })
 
 iap_alt.addEventListener("click", () => {
-  if (counter_i % 2 === 0) {
-    iap_alt.style.borderRadius = "50px";
-    iap_alt.style.boxShadow = "none";
-    iap_alt_p.style.borderRadius = "50px";
-    iap_alt_p.style.opacity = "1";
-    iap_alt_p.style.backdropFilter = "blur(10px)";
-  }
-  else {
-    iap_alt.style.borderRadius = "50%";
-    iap_alt.style.boxShadow = "inset -10px -5px 10px black, inset 10px 5px 20px white";
-    iap_alt_p.style.borderRadius = "50%";
-    iap_alt_p.style.opacity = "0";
-    iap_alt_p.style.backdropFilter = "none";
-  }
-  counter_i++;
+	if (counter_i % 2 === 0) {
+		iap_alt.style.borderRadius = "50px";
+		iap_alt.style.boxShadow = "none";
+		iap_alt_p.style.borderRadius = "50px";
+		iap_alt_p.style.opacity = "1";
+		iap_alt_p.style.backdropFilter = "blur(10px)";
+	}
+	else {
+		iap_alt.style.borderRadius = "50%";
+		iap_alt.style.boxShadow = "inset -10px -5px 10px black, inset 10px 5px 20px white";
+		iap_alt_p.style.borderRadius = "50%";
+		iap_alt_p.style.opacity = "0";
+		iap_alt_p.style.backdropFilter = "none";
+	}
+	counter_i++;
 })
 
 //click effect for Rhea
 let counter_r = 0;
 rhea.addEventListener("click", () => {
-  if (counter_r % 2 === 0) {
-    rhea.style.transform = "translate(22.5vw) scale(0.7, 0.7)";
-    rhea_text.style.opacity = "1";
-    rhea_text.style.transform = "translateX(-55%) scale(1.2, 1.2)";
-  }
-  else {
-    rhea.style.transform = "";
-    rhea_text.style.opacity = "0";
-    rhea_text.style.transform = "";
-  }
-  counter_r++;
+	if (counter_r % 2 === 0) {
+		rhea.style.transform = "translate(22.5vw) scale(0.7, 0.7)";
+		rhea_text.style.opacity = "1";
+		rhea_text.style.transform = "translateX(-55%) scale(1.2, 1.2)";
+	}
+	else {
+		rhea.style.transform = "";
+		rhea_text.style.opacity = "0";
+		rhea_text.style.transform = "";
+	}
+	counter_r++;
 })
 
 rhea_alt.addEventListener("click", () => {
-  if (counter_r % 2 === 0) {
-    rhea_alt.style.borderRadius = "50px";
-    rhea_alt.style.boxShadow = "none";
-    rhea_alt_p.style.borderRadius = "50px";
-    rhea_alt_p.style.opacity = "1";
-    rhea_alt_p.style.backdropFilter = "blur(10px)";
-  }
-  else {
-    rhea_alt.style.borderRadius = "50%";
-    rhea_alt.style.boxShadow = "inset -10px -5px 10px black, inset 10px 5px 20px white";
-    rhea_alt_p.style.borderRadius = "50%";
-    rhea_alt_p.style.opacity = "0";
-    rhea_alt_p.style.backdropFilter = "none";
-  }
-  counter_r++;
+	if (counter_r % 2 === 0) {
+		rhea_alt.style.borderRadius = "50px";
+		rhea_alt.style.boxShadow = "none";
+		rhea_alt_p.style.borderRadius = "50px";
+		rhea_alt_p.style.opacity = "1";
+		rhea_alt_p.style.backdropFilter = "blur(10px)";
+	}
+	else {
+		rhea_alt.style.borderRadius = "50%";
+		rhea_alt.style.boxShadow = "inset -10px -5px 10px black, inset 10px 5px 20px white";
+		rhea_alt_p.style.borderRadius = "50%";
+		rhea_alt_p.style.opacity = "0";
+		rhea_alt_p.style.backdropFilter = "none";
+	}
+	counter_r++;
 })
 
 //Mimas click effect
 let counter_m = 0;
 mimas.addEventListener("click", () => {
-  if (counter_m % 2 === 0) {
-    mimas.style.transform = "translate(-22.5vw) scale(0.7, 0.7)";
-    mimas_text.style.opacity = "1";
-    mimas_text.style.transform = "translateX(55%) scale(1.2, 1.2)";
-  }
-  else {
-    mimas.style.transform = "";
-    mimas_text.style.opacity = "0";
-    mimas_text.style.transform = "";
-  }
-  counter_m++;
+	if (counter_m % 2 === 0) {
+		mimas.style.transform = "translate(-22.5vw) scale(0.7, 0.7)";
+		mimas_text.style.opacity = "1";
+		mimas_text.style.transform = "translateX(55%) scale(1.2, 1.2)";
+	}
+	else {
+		mimas.style.transform = "";
+		mimas_text.style.opacity = "0";
+		mimas_text.style.transform = "";
+	}
+	counter_m++;
 })
 
 mimas_alt.addEventListener("click", () => {
-  if (counter_m % 2 === 0) {
-    mimas_alt.style.borderRadius = "50px";
-    mimas_alt.style.boxShadow = "none";
-    mimas_alt_p.style.borderRadius = "50px";
-    mimas_alt_p.style.opacity = "1";
-    mimas_alt_p.style.backdropFilter = "blur(10px)";
-  }
-  else {
-    mimas_alt.style.borderRadius = "50%";
-    mimas_alt.style.boxShadow = "inset -10px -5px 10px black, inset 10px 5px 20px white";
-    mimas_alt_p.style.borderRadius = "50%";
-    mimas_alt_p.style.opacity = "0";
-    mimas_alt_p.style.backdropFilter = "none";
-  }
-  counter_m++;
+	if (counter_m % 2 === 0) {
+		mimas_alt.style.borderRadius = "50px";
+		mimas_alt.style.boxShadow = "none";
+		mimas_alt_p.style.borderRadius = "50px";
+		mimas_alt_p.style.opacity = "1";
+		mimas_alt_p.style.backdropFilter = "blur(10px)";
+	}
+	else {
+		mimas_alt.style.borderRadius = "50%";
+		mimas_alt.style.boxShadow = "inset -10px -5px 10px black, inset 10px 5px 20px white";
+		mimas_alt_p.style.borderRadius = "50%";
+		mimas_alt_p.style.opacity = "0";
+		mimas_alt_p.style.backdropFilter = "none";
+	}
+	counter_m++;
 })
